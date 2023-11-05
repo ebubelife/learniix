5ef913a5947e22334b128fdd992c80fe69021bba
Ebube Life <ebubeemeka19@gmail.com>
2023-11-05 05:33:24 +0100

    updated signup and signin pages

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,5 +1,168 @@
-export default function SignIn() {
+"use client"
 
-    return(<></>)
+import Image from 'next/image'
+import Link from 'next/link'
+import Header from '../components/Header'
+import FAQs from '../components/Faqs'
+import Footer from '../components/Footer';
+import AppBar from '../components/Appbar';
 
+
+import { useState, useEffect, forwardRef, Fragment } from "react";
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Slide from '@mui/material/Slide';
+import { TransitionProps } from '@mui/material/transitions';
+
+
+const Transition = forwardRef(function Transition(
+  props: TransitionProps & {
+    children: React.ReactElement<any, any>;
+  },
+  ref: React.Ref<unknown>,
+) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+export default function Signin() {
+
+
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  
+  const handleClose = () => {
+    setOpen(false);
+  };
+  
+
+  return (
+
+    <>
+   {/*Pop to display options for getting started*/}
+<Fragment>
+     
+     <Dialog
+       open={open}
+       TransitionComponent={Transition}
+       keepMounted
+       onClose={handleClose}
+       aria-describedby="alert-dialog-slide-description"
+     >
+       <DialogTitle className="font-medium text-sm">{"Create An Account On Learniix"}</DialogTitle>
+       <DialogContent>
+        <div className="bg-black p-2 rounded-md text-white text-center shadow-xl hover:shadow-2xl cursor-pointer font-light">
+          Become An Affiliate
+        </div>
+
+        <div className="bg-green-500 p-2 rounded-md text-white text-center mt-4 shadow-xl hover:shadow-2xl cursor-pointer font-light">
+          Become A Vendor
+        </div>
+         
+       </DialogContent>
+      
+     </Dialog>
+   </Fragment>
+
+
+    <main className="items-center h-screen w-screen">
+
+      <div className='md:block hidden'>
+{/*main top header large screens*/}
+      <Header />
+
+
+
+      </div>
+
+      <div className='md:hidden '>
+
+        {/*main top header small screens*/}
+
+      <AppBar />
+
+      </div>
+      
+
+      
+
+
+      <div className="md:h-2/3 h-[500px] w-screen  " style={{ backgroundImage: "url('/images/home/Untitled design (7).png')" }}>
+
+{/*Hero */}
+      
+
+<div className='w-full md:h-2/3 h-[500px]  absolute z-10  grid md:grid-cols-2 grid-cols-1 md:py-2 py-8' style={{ backgroundColor:`rgba(0,0,0,0.8)`}}>
+
+<div className='h-full w-full md:px-20 md:py-10 md:p-0 p-4'>
+       <h1 className='text-green-600 md:text-4xl text-xl font-bold md:text-left text-center'>Become An Affiliate</h1>
+
+       <h1 className='text-white text-md md:text-left text-center font-smibold mt-4 '>Join our prestigious network of affiliates, get trained and gain access to a market of useful products. Promote the products using different media and earn up to 50% in commissions. </h1>
+       
+       {/*mobile version shows only on mobile */}
+     <div className="md:mt-4 mt-8 flex  justify-center md:hidden ">
+     <div className=' rounded-md shadow-md w-60 bg-white text-center font-semibold text-green-600 text-md  py-2 '>Great Commissions</div>
+
+     </div>
+
+ {/*large screen version shows only on large */}
+     <div className="md:mt-4 mt-8 hidden md:block  ">
+     <div className='h-10 rounded-md shadow-md w-60 bg-white text-center font-semibold text-green-600 text-xl  py-1 '>Great Commissions</div>
+
+     </div>
+
+
+       <h1 className='text-white md:text-md text-sm font-light mt-4 md:text-left text-center  '>Acquire premium salesmanship skills from our well crafted sales course and earn profitably by selling useful products for high commissions. </h1>
+      
+
+      
+      
+       </div>
+
+     
+       
+
+       <div>
+       <img src="/images/home/360_F_299939547_CwlOeI0nQ0ZACCpGmDrz2ZmOqsTfcvBf-removebg-preview.png" className='hero-image h-full w-full'  alt="hero-image" />
+
+       </div>
+
+</div>
+
+      </div>
+
+
+
+
+<p className='text-center text-3xl text-grey_600 mt-10 font-semibold text-green-600'>
+FAQs
+ 
+</p>
+
+<p className='text-center font-light text-sm text-grey_600'>
+Common Questions People Ask About Us
+ 
+</p>
+
+<div className="lg:px-40 mt-6 px-6 sm:px-6">
+<FAQs />
+
+</div>
+
+<div className="flex-grow">
+
+</div>
+
+<Footer />
+
+          </main>
+
+          </>
+  )
 }

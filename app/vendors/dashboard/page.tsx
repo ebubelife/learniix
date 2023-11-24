655550c6636f8d5d19c4d9c9fdd4ba04f9e63dff
ebubelife <ebubeemeka19@gmail.com>
2023-11-24 16:20:32 +0100

    ...

---
+++ app/vendors/dashboard/page.tsx
@@ -0,0 +1,148 @@
+"use client"
+// Import necessary React and Material-UI components
+import React, { useState,useRef } from 'react';
+
+import Link from 'next/link';
+import { AppBar, Toolbar, IconButton, Drawer, List, ListItem, ListItemText, Menu, MenuItem } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import { useRouter } from 'next/navigation';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import { useTheme,useMediaQuery } from '@mui/material';
+import AffiliateDashboardHeader from '@/app/affiliates/dashboard/header/page';
+
+
+
+// Your main component
+const AffiliateDashboard = () => {
+
+  
+
+
+  return (
+    <div >
+      <AffiliateDashboardHeader title="Affiliate Dashboard" />
+
+      <div className="w-screen h-screen px-4 py-4">
+
+
+      
+
+
+       <div className="mt-6"></div>
+
+<div className='w-screen  px-10'>
+
+<div className="grid md:grid-cols-4  gap-4 grid-cols-1">
+
+
+<div className='h-28 shadow-md rounded-md border border-1 border-zinc-300 p-2'>
+
+<p className='font-medim text-md ml-auto  mt-2'>Today&apos;s Sales</p>
+<div className='flex mt-2'>
+<div className='h-[50px] w-[50px] bg-green-500 rounded-md shadow-xl'>
+
+</div>
+
+
+
+<p className='text-xl font-medium ml-4 mt-2'>$500</p>
+
+
+</div>
+</div>
+
+
+<div className='h-28 shadow-md rounded-md border border-1 border-zinc-300 p-2'>
+
+<p className='font-medim text-md ml-auto  mt-2'>Today&apos;s Earnings</p>
+<div className='flex mt-2'>
+<div className='h-[50px] w-[50px] bg-green-500 rounded-md shadow-xl'>
+
+</div>
+
+
+
+<p className='text-xl font-medium ml-4 mt-2'>$500</p>
+
+
+</div>
+</div>
+
+<div className='h-28 shadow-md rounded-md border border-1 border-zinc-300 p-2'>
+
+<p className='font-medim text-md ml-auto  mt-2'>Today&apos;s Sales</p>
+<div className='flex mt-2'>
+<div className='h-[50px] w-[50px] bg-green-500 rounded-md shadow-xl'>
+
+</div>
+
+
+
+<p className='text-xl font-medium ml-4 mt-2'>$500</p>
+
+
+</div>
+</div>
+
+
+<div className='h-28 shadow-md rounded-md border border-1 border-zinc-300 p-2'>
+
+<p className='font-medim text-md ml-auto  mt-2'>Today&apos;s Sales</p>
+<div className='flex mt-2'>
+<div className='h-[50px] w-[50px] bg-green-500 rounded-md shadow-xl'>
+
+</div>
+
+
+
+<p className='text-xl font-medium ml-4 mt-2'>$500</p>
+
+
+</div>
+</div>
+
+
+
+
+</div>
+
+
+       
+       </div>
+
+
+       <div className='w-screen  flex justify-center'>
+
+<div className='h-60 w-1/3 mt-2 bg-zinc-200 rounded-md mt-4 grid place-content-center'>
+
+    <p className='text-zinc-400 text-xl'>Challenge Banner</p>
+
+</div>
+
+
+</div>
+       
+
+     
+    
+
+    
+
+
+      {
+        /* <div className="preview">
+        <ReactMarkdown>{markdown}</ReactMarkdown>
+      </div>
+
+        */
+      }
+     
+    </div>
+       
+       </div>
+ 
+  );
+};
+
+export default AffiliateDashboard

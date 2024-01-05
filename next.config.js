bf0f3b08f2356d1c13e785b23e353a7743dcdf3d
ebubelife <ebubeemeka19@gmail.com>
2024-01-05 12:27:56 +0100

    ...

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
+      },
+      images: {
+        domains: ['back.learniix.com'],
+      },
+}
 
 module.exports = nextConfig

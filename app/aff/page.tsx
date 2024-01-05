3ec8255eb81ca7fb32073672e65961a9474ce133
ebubelife <ebubeemeka19@gmail.com>
2024-01-05 12:44:19 +0100

    ...

---
+++ app/aff/page.tsx
@@ -0,0 +1,46 @@
+import { useEffect } from 'react';
+import Cookies from 'js-cookie';
+import axios from 'axios';
+
+interface VerifyAffiliateProps {
+  query: {
+    aff_id?: string;
+    pid?: string;
+  };
+}
+
+export default function VerifyAffiliate({ query }: VerifyAffiliateProps) {
+  useEffect(() => {
+    const { aff_id: affiliateID, pid: productID } = query;
+
+    const saledDetails = { affiliateID, productID };
+    Cookies.set('sales_details', JSON.stringify(saledDetails));
+
+    const fetchData = () => {
+      axios
+        .get(`https://back.learniix.com/api/view/product/` + productID)
+        .then((response) => {
+          const productSalesPageLink = response.data.productSalesPageLink;
+          window.location.href = productSalesPageLink;
+        })
+        .catch((error) => {
+          console.error(error);
+          window.location.href = 'https://learniix.com/data_error';
+        });
+    };
+
+    if (productID) {
+      fetchData();
+    }
+  }, []);
+
+  return null;
+}
+
+export async function getServerSideProps({ query }: { query: any }) {
+  return {
+    props: {
+      query,
+    },
+  };
+}
---
+++ app/data_error/page.tsx
@@ -0,0 +1,70 @@
+import { Button } from '@mui/material';
+
+import Cookies from 'js-cookie'
+import { GetServerSideProps, GetServerSidePropsContext } from 'next';
+import axios, { AxiosError, AxiosResponse } from 'axios';
+import Link from 'next/link';
+import React, { useEffect, useState } from 'react';
+
+import { useRouter } from 'next/router';
+
+
+export default function NoData(){
+
+   
+    return(
+    
+        <>
+       
+     <div className="w-screen h-screen  bg-white  ">
+         <div className="w-screen h-20  flex justify-center shadow-2xl">
+          
+       
+         
+
+         <img src={"/images/logo.png"} height={40} width={40} alt={"course-image"} className="h-20 w-40"/>
+      
+           
+         </div>
+
+         <div className="w-screen  flex justify-center  lg:pt-20 pt-4 sm:pt-4">
+
+            <div className="success-box p-4 text-center shadow-2xl  bg-white lg:w-1/3">
+
+                <p className="text-xl font-semibold text-gold">An Error Occured!!!</p>
+               
+                {
+                
+              <>
+                <p className="text-grey_600 mt-4"> Either <b>affiliate code</b> or the  <b>product code</b> is missing. Please try again or contact support </p>
+
+<p className="text-grey_600 mt-4"> Go to <Link href={"https://learniix.com"}> learniix.com</Link>   for more information on how our affiliate system works. </p>
+
+<div className="w-full mt-7 flex justify-center">
+              <Link href={"https://learniix.com"} className="bg-gold text-white hover:bg-white hover:text-grey_600 rounded-full text-sm p-3 shadow-xl">
+             Back To Home
+                </Link>
+
+              </div>
+
+              </>
+             
+                
+                
+                }
+
+
+           
+               
+
+
+            </div>
+
+
+         </div>
+        </div>
+
+   
+     </>
+    )
+}

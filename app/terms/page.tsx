a10f2c0e87aec01195b2d7cb28ec2610a3e14443
ebubelife <ebubeemeka19@gmail.com>
2024-01-12 16:06:40 +0100

    ...

---
+++ app/terms/page.tsx
@@ -0,0 +1,174 @@
+"use client"
+
+import Image from 'next/image'
+import Link from 'next/link'
+import Header from '../components/Header'
+import FAQs from '../components/Faqs'
+import Footer from '../components/Footer';
+import AppBar from '../components/Appbar';
+
+export default function TOS(){
+
+
+    return (<>
+     <div className="main  w-screen bg-grey_100 pb-10 bg-white " >
+     <div className="lg:block hidden sm:hidden ">
+  <Header />   
+    
+  </div>
+
+  <div  className="lg:hidden block sm:block">
+  <AppBar />
+
+  </div>
+    <div className="flex justify-center lg:py-10 ">
+   
+
+
+        <div className="text-grey_600 lg:w-2/3 shadow-xl lg:py-10 lg:px-7 px-4 sm:px-4">
+
+            <p>
+
+          <p className="text-green mt-4 font-semibold text-xl">Zenithstake Terms & Conditions</p> 
+
+
+
+       
+
+<p>TERMS AND CONDITIONS, </p>
+<p className="mt-2">We acknowledge, represent, and accept that you have read and agreed to comply by the terms, restrictions, and regulations described here on Learniix. These regulations guide and direct your use of the website. Thus you must comprehend every information contained therein.
+</p>
+
+<p className="mt-2 font-semibold">BUYERS </p>
+
+
+
+<p className="mt-2 ">
+The following conditions are what you are expected to comply to as a buyer upon registering on the Learniix platform, these are:
+
+Easy Access to Marketplace: As a buyer, you can easily access the marketplace and choose what you want.
+
+Non-Resale Rights: As a buyer, you’re expected to purchase a product for private use. You’re expected not to resell, alter, or revamp any purchased product
+Money-back Guarantee: There is 30-day money-back guarantee on any listed product on this platform.
+
+Personal Liability: You must not divulge in any security information about your account to anyone. You are solely responsible for any actions that may result from disclosing your credentials.
+
+</p>
+
+
+
+
+<p className="mt-2 font-semibold">AFFILIATES</p>  
+
+<p className="mt-2 ">
+The following conditions are what you are expected to adhere to as an affiliate upon registering on the Learniix platform, these are:
+</p>
+
+<p className="mt-2 ">
+
+1a. Access to Marketplace: Affiliates can only use Learniix after paying a fee of ₦10,000 (or its equivalent in dollars) per year to subscribe to the platform. Otherwise, affiliates have no access to the Learniix marketplace.
+</p>
+
+<p className="mt-2 ">
+1b. As an affiliate: You have access to an an ever-increasing array of incredible digital products available for sales/promotion, Different tools you can use to make money on the platform and every support you need to earn massively on this platform
+
+    </p>
+
+    <p className="mt-2 ">
+    Personal Liability: On no account should you divulge security information about your account to anyone. You are solely responsible for any actions that may result from disclosing your credentials.
+
+        </p>
+
+        <p className="mt-2 ">
+        Payment Process: As an affiliate, you are paid according to your sales commissions and for every sales there is an EXTRA-BONUS ADDED FOR YOU AS AN AFFILIATE ON THIS EARNING PLATFORM.
+
+            </p>
+
+            <p className="mt-2 ">
+            Adherence to Local & International Customer Regulations: When promoting a Learniix-powered product, all advertising, marketing, mailing, or promotion must comply with the CAN-SPAM Act, the FTC Act, the FTC’s rules on testimonials and earnings claims, and it must include a functional unsubscribe link that enables the recipient to stop receiving communications at any time.
+
+                </p>
+
+                <p className="mt-2 ">
+                Confidentiality: Respect the privacy of the data that has been disclosed, created, or made available to you, including information and user/customer data.
+
+                    </p>
+
+                    <p className="mt-2 ">
+     Withdrawals: All payment will be paid via a Nigerian bank deposit. Take note that you’re earning in dollars but you will be paid in your local currency
+</p>
+
+
+<p className="mt-2 ">
+Miscellaneous: Affiliates might get extra bonuses depending on reached goals or other factors. Additionally, several technical training opportunities, etc., are made accessible for affiliate use.
+
+</p>
+
+
+<p className="mt-4 font-semibold">VENDORS </p>
+
+<p className="mt-2 ">
+
+The following conditions are what you are expected to adhere to as a Vendor upon registering on the Learniix platform, these are:
+
+</p>
+
+<p className="mt-2 ">
+
+Product Listing, Vetting & Approval: As a vendor, you must register on the platform to gain access to the marketplace, before you can list your product. So, once you register on the platform then you can submit your product for listing.
+
+</p>
+
+<p className="mt-2 ">
+
+Payment Process: Learniix pays its vendors via bank transfers. Your money is sent to a valid bank account with your name. Vendors and affiliates alike are paid every Sunday of the week.
+
+</p>
+
+<p className="mt-2 ">
+
+Commission Percentage Policy: The vendors decide on the percentage to set aside in commission for affiliates. However, we generally advise and make vendors pay reasonable commissions to the affiliates to gear up interest in promoting their products.
+
+</p>
+
+<p className="mt-2 ">
+
+Intermediary between Vendor & Buyer: We offer a mediator service so that vendors can list their products on our platform for potential customers.
+
+</p>
+
+<p className="mt-2 ">
+
+Confidentiality: Respect the privacy of the data that has been disclosed, created, or made available to you, including information and user/customer data.
+
+</p>
+
+
+<p className="mt-2 ">
+
+Personal Liability: On no account should you divulge security information about your account to anyone. You are solely responsible for any actions that may result from disclosing your credentials.
+
+</p>
+
+
+
+<p className="mt-2 ">
+
+Compliance: As a vendor, you must comply with every requested information on the product you intend to list on Learniix. Failure to comply would result in listing disapproval and other dire consequences.
+</p>
+
+
+
+
+
+
+          </p>
+
+        </div>
+    </div>
+    </div>
+    <Footer />
+    </>
+    )
+
+}

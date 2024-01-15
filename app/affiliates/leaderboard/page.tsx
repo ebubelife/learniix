ac0e02f0426c5ed51ed3c4497dfebb85cf327b5c
ebubelife <ebubeemeka19@gmail.com>
2024-01-15 15:19:33 +0100

    ...

--- app/affiliates/leaderboard/page.tsx
+++ app/affiliates/leaderboard/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 // Import necessary React and Material-UI components
-import React, { useState,useRef } from 'react';
+import React, { useState,useRef, useEffect } from 'react';
 
 import Link from 'next/link';
 import { AppBar, Toolbar, IconButton, Drawer, List, ListItem, ListItemText, Menu, MenuItem, InputAdornment, TextField } from '@mui/material';
@@ -18,13 +18,33 @@ import AffiliateDashboardHeader from '../dashboard/header/page';
 const Leaderboard = () => {
 
   
+  const [timeLeft, setTimeLeft] = useState({ days: 0, hours: 0 });
 
+  const calculateTimeLeft = () => {
+    const now = new Date();
+    const targetDate = new Date('2024-02-14'); // Set the target date (year-month-day)
+
+    const difference = targetDate.getTime() - now.getTime();
+    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+
+    setTimeLeft({ days, hours });
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      calculateTimeLeft();
+    }, 1000);
+
+    // Cleanup the interval on component unmount
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <div >
       <AffiliateDashboardHeader title="LeaderBoard" />
 
-      <div className="w-screen h-screen px-4 py-4 overflow-y">
+      <div className="w-screen h-screen  py-4 overflow-y">
 
 
       
@@ -36,18 +56,43 @@ const Leaderboard = () => {
 
         <div className=''>
 
-            <div className='shadow-xl p-4'>
+            <div className='shadow-xl '>
                 <p className='text-xl font-medium text-zinc-600'>LIB Sales Challenge Ranking</p>
 
-                <p className='text-md mt-4 font-medium text-zinc-600'>Time Left: <span className='text-green-500'>5 Days</span></p>
+                <div className='w-screen  flex justify-center'>
 
 
-                <div className=" mt-6 h-96 w-full bg-zinc-200 grid place-content-center rounded-md">
-
-
-<p>Challenge Banner</p>
+  
+<div className=' md:w-1/3 w-full mt-2 h-20 mt-4 rounded-md mt-4 grid place-content-center text-xl font-semibold text-green-500 shadow-md'>
+<p className='text-sm text-black text-center'>TIME LEFT</p>
+{timeLeft.days} days, {timeLeft.hours} hours
 
 </div>
+
+
+
+
+</div>
+
+
+
+      <div className='w-screen  flex justify-center'>
+
+{
+ 
+<div className=' md:w-1/3 mt-2 h-96 rounded-md mt-4 flex justify-center'>
+
+  <img src="/images/home/WhatsApp Image 2024-01-13 at 12.56.32 PM.jpeg" className='h-full w-full' alt="learniix promo banner" />
+
+</div>
+
+}
+
+
+</div>
+
+
+
        
             </div>
 
@@ -56,7 +101,7 @@ const Leaderboard = () => {
 
 
 
-        <div className=''>
+        <div className='hidden'>
 
 <div className='shadow-xl p-4'>
     <p className='text-xl font-medium text-zinc-600 mb-4'>Top 10 Sellers</p>

6df6f876793b8857c3fe521ff14b7c07966eae90
ebubelife <ebubeemeka19@gmail.com>
2024-01-27 20:20:22 +0100

    ...

--- app/buy/page.tsx
+++ app/buy/page.tsx
@@ -21,6 +21,7 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
+import { PaystackButton } from 'react-paystack';
 
 
 export default function Buy(){
@@ -66,10 +67,13 @@ export default function Buy(){
     const [customerName, setcustomerName] = React.useState();
     const [customerPhone, setcustomerPhone] = React.useState();
     const [customerEmail, setcustomerEmail] = React.useState();
+    const [paystackPublic, setPaystackPublick] = React.useState("pk_live_f27aa22ced88d8d87bee5c58b19625409bc4c7a8");
 
     const router = useRouter();
     const searchParams = useSearchParams();
    
+
+    
 
     const productID  = searchParams.get('pid');
     const affiliateID  = searchParams.get('aff_id');
@@ -123,6 +127,21 @@ export default function Buy(){
    }, [productID]);
 
 
+   //component props for Paystack
+   const componentProps =  {
+    reference:reference,
+    email,
+    amount:((amount +150)*100),
+    channels:['card'],
+
+   
+    
+  
+    text: "Complete Purchase",
+    onSuccess: () =>
+     {runAPI("NGN")},
+    onClose: () => {router.back()}
+  }
 
 
  
@@ -374,16 +393,8 @@ export default function Buy(){
 
           <div className="paystack-button bg-white border-2 border-grey_300  text-white hover:bg-white hover:text-grey_600 w-full py-2 shadow-xl rounded-xl mt-6 flex">
           <img src="https://learniix.com/images/Paystack_Logo.png" className="img-responsive h-10 w-28" alt="Paystack-zenithstake"/>
-          <button
-          className='text-green-500 font-semibold ml-4 text-sm'
-        onClick={() => {
+          <PaystackButton publicKey={paystackPublic} className="text-sm text-grey_600 hover:text-purple2 font-semibold ml-3" {...componentProps} text='Pay with Paystack' >Pay with Paystack</PaystackButton>
 
-          //  SquadPay();
-         
-        }}
-      >
-        Paystack (NGN)
-      </button>
 
 
 
